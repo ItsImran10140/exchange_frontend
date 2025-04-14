c5ba881d6c4dc9eabee731626f78c8adc3501f57
Imran Shah <imranshah10140@gmail.com>
2025-04-14 17:30:42 +0530

    some fix

--- app/components/Market/MarketGrid.tsx
+++ app/components/Market/MarketGrid.tsx
@@ -4,7 +4,7 @@ import { markets } from "../../constants/home";
 export const MarketGrid = () => (
   <>
     <h1 className="text-2xl font-bold mb-6">Markets</h1>
-    <div className="grid grid-cols-2 gap-4 w-full max-w-2xl">
+    <div className="grid grid-cols-2 mb-10 gap-4 w-full max-w-2xl">
       {markets.map((market) => (
         <MarketCard key={market} market={market} />
       ))}
