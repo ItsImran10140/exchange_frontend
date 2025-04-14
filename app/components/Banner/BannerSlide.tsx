e4cfedfc7f5119f2f0e3dd9b4b1004d488bf50c2
Imran Shah <imranshah10140@gmail.com>
2025-04-14 16:22:46 +0530

    Banner Text Update

--- app/components/Banner/BannerSlide.tsx
+++ app/components/Banner/BannerSlide.tsx
@@ -24,16 +24,19 @@ export const BannerSlide = ({ banner, index, isActive }: BannerSlideProps) => (
       className="rounded-lg"
     />
     
-    <div className="absolute inset-0 flex flex-col justify-center px-16">
-      <h1 className="text-4xl font-bold text-white mb-4" style={{ fontSize: '2rem' }}>{banner.title}</h1>
-      <p className="text-xl text-white mb-6" style={{ fontSize: '0.8rem' }}>{banner.description}</p>
+    <div className="absolute inset-0 flex flex-col justify-end items-start px-16 pb-10">
+      <div className="w-[50%]">
+
+      <h1 className="text-4xl  font-bold text-white mb-1" style={{ fontSize: '3rem' }}>{banner.title}</h1>
+      <p className="text-xl text-gray-300 mb-6" style={{ fontSize: '1.1rem' }}>{banner.description}</p>
       <div>
         <Link 
           href={banner.ctaLink} 
-          className="bg-white text-black text-sm font-medium py-1 px-6 rounded-full hover:bg-opacity-90 inline-block"
-        >
+          className="bg-white text-black text-md font-medium py-2 px-6 rounded-lg hover:bg-opacity-90 inline-block"
+          >
           {banner.ctaText}
         </Link>
+          </div>
       </div>
     </div>
   </div>
